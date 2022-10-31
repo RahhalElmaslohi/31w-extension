e72b965e1ddb3c1ff4b09b5c274dafb615fbe588
RahhalElmaslohi <rahhalelmaslohi@gmail.com>
2022-10-31 18:54:36 -0400

    ajout de la function ajouter_radio_carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -4,6 +4,7 @@ let elmBtnModale = document.querySelector(".btn_modale");
 let elmCarrousel = document.querySelector(".carrousel");
 let elmBtnModaleFermer = document.querySelector(".btn_fermer");
 let elmCarrousel__figure = document.querySelector(".carrousel__figure");
+let elmCarrousel__form = document.querySelector(".carrousel__form");
 
 
 /* ---------------------------------------- Les éléments de la galerie */
@@ -14,7 +15,8 @@ let elmCarrousel__figure = document.querySelector(".carrousel__figure");
   /* ----------------------------- Étape 1 parcourir les images de la galerie */
   for (const elmImg of elmGalerieImg) {
     console.log(elmImg.getAttribute("src"));
-    ajouter_img_carrousel(elmImg) 
+    ajouter_img_carrousel(elmImg)
+    ajouter_radio_carrousel() 
 
     
 }
@@ -33,6 +35,34 @@ let elmCarrousel__figure = document.querySelector(".carrousel__figure");
     elmCarrousel__figure.appendChild(elmCarrousel__figure__img);
   }
 
+   /**
+   * Ajoute un radio bouton dans le carrousel
+   */
+    function ajouter_radio_carrousel() {
+        let elmCarrousel__form__radio = document.createElement("input");
+        elmCarrousel__form__radio.setAttribute("name", "carrousel__form__radio");
+        elmCarrousel__form__radio.setAttribute("class", "carrousel__form__radio");
+        elmCarrousel__form__radio.setAttribute("type", "radio");
+        elmCarrousel__form__radio.dataset.index = index;
+        index++;
+        elmCarrousel__form.appendChild(elmCarrousel__form__radio);
+        /* ------------------------ écouteur sur radio pour afficher une nouvelle image */
+        elmCarrousel__form__radio.addEventListener("mousedown", function () {
+          console.log(this.dataset.index);
+    
+          if (dernierIndex != -1) {
+            elmCarrousel__figure.children[dernierIndex].classList.remove(
+              "carrousel__figure__img--activer"
+            );
+          }
+    
+          elmCarrousel__figure.children[this.dataset.index].classList.add(
+            "carrousel__figure__img--activer"
+          );
+          console.log(index);
+          dernierIndex = this.dataset.index;
+        });
+      }
 
 elmBtnModale.addEventListener("mousedown", function () {
     console.log("bouton boîte modale");
