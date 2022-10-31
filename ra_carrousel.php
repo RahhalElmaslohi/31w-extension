c70955da966b2e52245fb120142b450f96ab81c1
RahhalElmaslohi <rahhalelmaslohi@gmail.com>
2022-10-30 20:16:55 -0400

    ajout de css dans style

--- ra_carrousel.php
+++ ra_carrousel.php
@@ -11,7 +11,17 @@ Version: 1.0.0
 */
 
 function genere_boite(){
-    $contenu = "<div class = 'carrousel'>Carrousel</div>";
+    $contenu = "
+    <style>
+        .carrousel {
+            border:2px solid #aaa;
+            background-color:#0f0;
+            width:200px;
+            height:200px;
+        }
+    </style>    
+    
+    <div class='carrousel'>Carrousel</div>";
     return $contenu;
 
 }
